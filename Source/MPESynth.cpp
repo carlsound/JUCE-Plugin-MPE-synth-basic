a02ac1ac3c0cf7abca3cd0e87cbe86846da014cd
carlsound <john.carlson@carlsound.net>
2018-01-24 01:14:47 -0600

    Replaced MPEInsteumentListener with MPESynth
    Replaced MPEInsteumentListener with MPESynth
    
    derived from MPESynthesiser, instead of MPEInstrument : Listener

---
+++ Source/MPESynth.cpp
@@ -0,0 +1,126 @@
+/*
+  ==============================================================================
+
+    MPESynth.cpp
+    Created: 24 Jan 2018 12:33:41am
+    Author:  john_
+
+  ==============================================================================
+*/
+
+#include "MPESynth.h"
+
+MPESynth::MPESynth()
+{
+}
+
+MPESynth::MPESynth(MPEInstrument * instrument)
+{
+}
+
+MPESynth::~MPESynth()
+{
+}
+
+void MPESynth::clearVoices()
+{
+}
+
+int MPESynth::getNumVoices()
+{
+}
+
+MPESynthesiserVoice * MPESynth::getVoice(int index) const
+{
+	return nullptr;
+}
+
+void MPESynth::addVoice(MPESynthesiserVoice * newVoice)
+{
+}
+
+void MPESynth::removeVoice(int index)
+{
+}
+
+void MPESynth::reduceNumVoices()
+{
+}
+
+void MPESynth::turnOffAllVoices(bool allowTailOff)
+{
+}
+
+void MPESynth::setVoiceStealingEnabled(bool shouldSteal)
+{
+}
+
+bool MPESynth::isVoiceStealingEnabled() const noexcept
+{
+}
+
+void MPESynth::setCurrentPlaybackSampleRate(double newRate)
+{
+}
+
+void MPESynth::handleMidiEvent(const MidiMessage &)
+{
+}
+
+void MPESynth::handleController(int, int, int)
+{
+}
+
+void MPESynth::handleProgramChange(int, int)
+{
+}
+
+void MPESynth::noteAdded(MPENote newNote)
+{
+}
+
+void MPESynth::noteReleased(MPENote finishedNote)
+{
+}
+
+void MPESynth::notePressureChanged(MPENote changedNote)
+{
+}
+
+void MPESynth::notePitchbendChanged(MPENote changedNote)
+{
+}
+
+void MPESynth::noteTimbreChanged(MPENote changedNote)
+{
+}
+
+void MPESynth::noteKeyStateChanged(MPENote changedNote)
+{
+}
+
+void MPESynth::renderNextSubBlock(AudioBuffer<float>& outputAudio, int startSample, int numSamples)
+{
+}
+
+void MPESynth::renderNextSubBlock(AudioBuffer<double> &outputAudio, int startSample, int numSamples)
+{
+}
+
+MPESynthesiserVoice * MPESynth::findFreeVoice(MPENote noteToFindVoiceFor, bool stealIfNoneAvailable)
+{
+	return nullptr;
+}
+
+MPESynthesiserVoice * MPESynth::findVoiceToSteal(MPENote noteToStealVoiceFor)
+{
+	return nullptr;
+}
+
+void MPESynth::startVoice(MPESynthesiserVoice * voice, MPENote noteToStart)
+{
+}
+
+void MPESynth::stopVoice(MPESynthesiserVoice * voice, MPENote noteToStop, bool allowTailOff)
+{
+}
---
+++ Source/MPESynth.h
@@ -0,0 +1,61 @@
+/*
+  ==============================================================================
+
+    MPESynth.h
+    Created: 24 Jan 2018 12:33:41am
+    Author:  john_
+
+  ==============================================================================
+*/
+
+#pragma once
+#include "juce_audio_basics/buffers/juce_AudioSampleBuffer.h"
+#include "juce_audio_basics/midi/juce_MidiMessage.h"
+#include "juce_audio_basics/mpe/juce_MPENote.h"
+#include "juce_audio_basics/mpe/juce_MPESynthesiser.h"
+#include "juce_audio_basics/mpe/juce_MPEInstrument.h"
+#include "juce_audio_basics/mpe/juce_MPESynthesiserVoice.h"
+
+using namespace juce;
+
+class MPESynth : MPESynthesiser
+{
+public:
+	MPESynth();
+	explicit MPESynth(MPEInstrument *instrument);
+	~MPESynth()();
+
+	//==============================================================================
+
+	void clearVoices();
+	int getNumVoices();
+	MPESynthesiserVoice* getVoice(int index) const;
+	void addVoice(MPESynthesiserVoice* newVoice);
+	void removeVoice(int index);
+	void reduceNumVoices();
+	void turnOffAllVoices(bool allowTailOff);
+	void setVoiceStealingEnabled(bool shouldSteal);
+	bool isVoiceStealingEnabled() const noexcept;
+	void setCurrentPlaybackSampleRate(double newRate);
+	void handleMidiEvent(const MidiMessage&);
+	void handleController(int, int, int);
+	void handleProgramChange(int, int);
+
+	//==============================================================================
+
+protected:
+	void noteAdded(MPENote newNote);
+	void noteReleased(MPENote finishedNote);
+	void notePressureChanged(MPENote changedNote);
+	void notePitchbendChanged(MPENote changedNote);
+	void noteTimbreChanged(MPENote changedNote);
+	void noteKeyStateChanged(MPENote changedNote);
+	void renderNextSubBlock(AudioBuffer<float> &outputAudio, int startSample, int numSamples);
+	void renderNextSubBlock(AudioBuffer<double> &outputAudio, int startSample, int numSamples);
+	MPESynthesiserVoice* findFreeVoice(MPENote noteToFindVoiceFor, bool stealIfNoneAvailable);
+	MPESynthesiserVoice* findVoiceToSteal(MPENote noteToStealVoiceFor);
+	void startVoice(MPESynthesiserVoice* voice, MPENote noteToStart);
+	void stopVoice(MPESynthesiserVoice* voice, MPENote noteToStop, bool allowTailOff);
+
+private:
+};
